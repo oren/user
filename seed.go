670a03689b04e7a10f6d18f09ab4739e060be2e0
Oren <orengolan@gmail.com>
2015-11-01 15:25:22 -0800

    db package

---
+++ seed.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/google/cayley"
+	_ "github.com/google/cayley/graph/bolt"
+	"log"
+)
+
+import "github.com/google/cayley/graph"
+
+func open() {
+	path := "/tmp/user-db"
+	// Initialize the database
+	graph.InitQuadStore("bolt", path, nil)
+
+	// Open and use the database
+	store, err := cayley.NewGraph("bolt", path, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	store.AddQuad(cayley.Quad("person:sophie", "type", "Person", ""))
+	store.AddQuad(cayley.Quad("person:sophie", "name", "Sophie Grégoire", ""))
+	store.AddQuad(cayley.Quad("person:sophie", "born", "1974", ""))
+	store.AddQuad(cayley.Quad("person:sophie", "lives in", "country:canada", ""))
+
+	store.AddQuad(cayley.Quad("person:justin", "type", "Person", ""))
+	store.AddQuad(cayley.Quad("person:justin", "name", "Justin Trudeau", ""))
+	store.AddQuad(cayley.Quad("person:justin", "born", "1972", ""))
+	store.AddQuad(cayley.Quad("person:justin", "lives in", "country:canada", ""))
+	store.AddQuad(cayley.Quad("person:justin", "in love with", "person:sophie", ""))
+}
+
+func main() {
+	open()
+}
+
+func Register() {
+	log.Println("register function")
+}
--- server.go
+++ server.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/oren/user/db"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/health", health)
+	http.HandleFunc("/register", register)
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "ok")
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	db.Register()
+	fmt.Fprintf(w, "register")
 }
